d2ceb1607b6959d6cf355482f069de10ddc4b9ef
MoSalah07 <notway10@yahoo.com>
2023-02-07 12:34:04 +0200

    clone-google

---
+++ components/layout/Layout.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Head from "next/head";
+import Footer from "../footer/Footer";
+import Header from "../header/Header";
+
+function Layout({ title = 'Google-V3', children }) {
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+}
+
+export default Layout;
